186a4a13dd405d654e2c314cc60cbb62cdb41c8a
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-10 11:11:13 +0700

    build: 🌼 v0.0.1 (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,59 @@
-console.log('Chao xìn 🇻🇳');
+import { createLogger, transports, format } from 'winston';
+import { flatten } from 'lodash';
+
+type Level = 'error' | 'warn' | 'info';
+type DefaultMeta = {
+  layer?: string;
+  component?: string;
+  fileName?: string;
+};
+
+const getLogger = (level: Level = 'info', defaultMeta?: DefaultMeta) => {
+  const { Console } = transports;
+  const {
+    combine, timestamp, json, splat, errors,
+  } = format;
+  const logger = createLogger({
+    level,
+    format: combine(
+      errors({ stack: true }),
+      splat(),
+      timestamp(),
+      json(),
+    ),
+    defaultMeta,
+    transports: [
+      new Console(),
+    ],
+  });
+  return logger;
+};
+
+const formatRest = (...rest: any[]) => {
+  const flattenRest = flatten(rest);
+  flattenRest.forEach((element: any, index: number) => {
+    if (element instanceof Error) {
+      flattenRest.splice(index, 1);
+      flattenRest.push(`${element.name}: ${element.message}`);
+      // if (config.nodeEnv === 'development') {
+      //   flattenRest.push(element.stack);
+      // }
+      flattenRest.push(element.stack);
+    }
+  });
+  return flattenRest;
+};
+
+const logger = (defaultMeta?: DefaultMeta) => ({
+  error(...rest: any[]) {
+    getLogger('error', defaultMeta).error(formatRest(rest));
+  },
+  warn(...rest: any[]) {
+    getLogger('warn', defaultMeta).warn(formatRest(rest));
+  },
+  info(...rest: any[]) {
+    getLogger('info', defaultMeta).info(formatRest(rest));
+  },
+});
+
+export { logger };
