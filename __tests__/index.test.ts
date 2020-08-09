b173c517d98bcde373922f6349252bc258db04ca
Cuong Tran <103cuong@gmail.com>
2020-08-09 22:14:52 +0700

    feat(v0.1.0): support global options (#7)

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,3 +1,10 @@
+import { diana } from 'diana-js';
+import { logger, globalOptions } from '../src';
+
+globalOptions.environment = 'production';
+
 it('test', async() => {
-  expect(true).toEqual(true);
+  expect(typeof logger).toEqual('function');
+  expect(typeof logger()).toEqual('object');
+  logger().info('id', diana());
 });
